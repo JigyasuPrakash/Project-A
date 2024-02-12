941e3ee1337c68bb1185ac2ed7261d7d52439dcd
imsrish <imsrish09@gmail.com>
2024-02-12 17:56:20 +0530

    idk

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const connection = mysql.createConnection({
     password: 'DcLhsiyM8b',
     database: 'sql6681899'
 });
+
 connection.connect((err) => {
     if (err) {
         console.error('MySQL connection error:', err);
@@ -31,6 +32,48 @@ app.get('/', (req, res) => {
     });
 });
 
+// app.get('/sql', (req, res) => {
+    
+//     const query1 = "SELECT * FROM events WHERE id=2;";
+//     const query2 = "SELECT * FROM events;";
+
+//     connection.query(query1, (err, results) => {
+//         if (err) {
+//             console.error('Error executing MySQL query:', err);
+//             res.status(500);
+//             res.send('Internal Server Error');
+//         } else {
+//             res.status(200);
+//             res.json(results);
+//         }
+    
+//     });
+        
+// });
+
+// app.get('/',(req, res) => {
+//     connection.connect();
+//     var sql = 'SELECT * FROM investors?; SELECT * FROM member_info?;'
+//     connection.query(sql, function(err, results, fields){
+//         if (!err) {
+//             // res.send(JSON.stringify(results[0]));
+//             // res.send(JSON.stringify(results[1]));
+//             console.log('hey');
+//             //console.log(results);
+//             console.log(results[0]);
+//             console.log(results[1]);
+//         }   else{
+//             console.log('Error while performing query.');
+//         }
+//     });
+//     connection.end();
+// })
+// //app.listen(port, () => console.log('Server Started pn port ${port}'));
+// app.listen(3002);
+
+
+
+
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`);
 })
