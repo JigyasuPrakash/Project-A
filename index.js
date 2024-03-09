90a50dd79e67309e8243aff17b3060bec81f0a5a
imsrish <imsrish09@gmail.com>
2024-03-09 14:55:20 +0530

    category and organizer post

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000;
 const { db } = require("./firebase");
 
+app.use(express.json());
 app.get('/', (req, res) => {
     res.send("Welcome to my Project Alpha ☺️");
 });
@@ -70,6 +71,60 @@ app.get('/event', (req, res) => {
             res.send("Something went wrong, please try again");
         })
 });
+
+app.post('/organizer',(req,res)=> {
+    let poc_name = req.body.poc_name;
+    let poc_contact= req.body.poc_contact;
+    let poc_email = req.body.poc_email;
+    let comittee= req.body.comittee;
+
+    let message="";
+
+    if((poc_name != "") && (comittee != "") && (poc_email != "") && (poc_contact != null)){
+        res.code=200;
+        message="success";
+    }
+    
+    else{
+        res.code=400;
+        message="incomplete";
+    }
+
+    if(res.code ==200){
+        
+        let organizer = db.collection("Organizer");
+        organizer.add ({poc_name: poc_name , comittee:comittee,poc_contact:poc_contact,poc_email:poc_email});
+    }
+    res.send(message);
+   
+}) 
+
+app.post('/category',(req,res)=> {
+    let name = req.body.name;
+    let sponser= req.body.sponser;
+    let audience_size = req.body.audience_size;
+    let duration= req.body.duration;
+
+    let message="";
+
+    if((name != "") && (sponser != null ) && (audience_size != "") && (duration != null)){
+        res.code=200;
+        message="success";
+    }
+    
+    else{
+        res.code=400;
+        message="incomplete";
+    }
+
+    if(res.code ==200){
+        
+        let category = db.collection("Category");
+        category.add ({name: name , sponser: sponser,audience_size:audience_size,duration:duration});
+    }
+    res.send(message);
+   
+}) 
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
