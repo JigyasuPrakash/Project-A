0a53ad069f1d9af0c76eb6ebde5aa3988b4cb521
JigyasuAnchanto <jigyasu.prakash@anchanto.com>
2024-03-10 23:12:39 +0530

    event handler added

---
+++ handlers/eventHandler.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const router = express.Router();
+const { db, Timestamp } = require("../firebase");
+
+router.get('/', (req, res) => {
+    const event = db.collection('Event');
+    event.get()
+        .then((snapshot) => {
+            const data = snapshot.docs.map((doc) => ({ id: doc.id, name: doc.get("name"), fee: doc.get("fee"), start_date: doc.get("start_date"), end_date: doc.get("end_date"), category_id: doc.get("category_id"), organizer_id: doc.get("organizer_id"), venue_id: doc.get("venue_id"), year: doc.get("year") }));
+            console.log(data);
+            res.status(200);
+            res.send(data);
+        })
+        .catch((error) => {
+            console.error(error);
+            res.status(500);
+            res.send("Something went wrong, please try again");
+        })
+});
+
+router.post('/', (req, res) => {
+    let name = req.body.name;
+    let start_date = req.body.start_date;
+    let end_date = req.body.end_date;
+    let fee = req.body.fee;
+    let year = req.body.year;
+    let message = "";
+
+    if (name !== "" && start_date && end_date && fee) {
+        if (isValidDate(start_date) && isValidDate(end_date)) {
+            start_date = Timestamp.fromDate(new Date(start_date));
+            end_date = Timestamp.fromDate(new Date(end_date));
+            res.code = 200;
+            message = "success";
+        } else {
+            res.code = 400;
+            message = "Invalid date format";
+        }
+    }
+    else {
+        res.code = 400;
+        message = "incomplete";
+    }
+    function isValidDate(dateString) {
+        return !isNaN(Date.parse(dateString));
+    }
+    
+    if (res.code == 200) {
+        let event = db.collection("Event");
+        console.log({ name: name, start_date: start_date, end_date: end_date, fee: fee, year: year })
+        event.add({ name: name, start_date: start_date, end_date: end_date, fee: fee, year: year });
+    }
+    res.send(message);
+})
+
+module.exports = router;
